aaf224fc328b41a5f876086b257cd630afc6022b
Liang Geng <geng.161@osu.edu>
2023-08-19 15:12:21 -0400

    Change config

--- source_codes/Waffle_dataset/parameters.h
+++ source_codes/Waffle_dataset/parameters.h
@@ -11,7 +11,7 @@ const double range_size_percent_max = 1.5;
 const uint64_t range_or_knn_frequency = 10; // insert or delete / query
 
 const int precision = 10;
-const int query_per_tick = 2000000;
+const int query_per_tick = 200000;
 const int ticks_per_episode = 20;
 const int k = 10;
 const int random_move_trial = 10; // number of hops from one street to other streets
